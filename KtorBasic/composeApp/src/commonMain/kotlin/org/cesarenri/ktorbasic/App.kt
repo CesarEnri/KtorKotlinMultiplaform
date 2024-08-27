30395add35db871b54c8e35c61190dd6c9fed96c
Enrique Beltran <cesar78200921@hotmail.com>
2024-08-27 12:10:44 -0400

    - Ktor

--- KtorBasic/composeApp/src/commonMain/kotlin/org/cesarenri/ktorbasic/App.kt
+++ KtorBasic/composeApp/src/commonMain/kotlin/org/cesarenri/ktorbasic/App.kt
@@ -1,37 +1,66 @@
 package org.cesarenri.ktorbasic
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
+import network.NetworkUtils
+import network.model.ApiResponse
+import network.model.Hero
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import ktorbasic.composeapp.generated.resources.Res
-import ktorbasic.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var superheroName by remember { mutableStateOf("") }
+        var superHeroList by remember { mutableStateOf<List<Hero>>(emptyList()) }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Row { TextField(value = superheroName, onValueChange = { superheroName = it})
+                Button(onClick = {getSuperHeroList(superheroName){superHeroList = it} })
+                {
+                    Text("Load")
+                }
+             }
+            //list
+            LazyColumn {
+                items(superHeroList){
+                    hero -> Text(hero.name)
                 }
             }
+
         }
+    }
+}
+
+fun getSuperHeroList(superheroName: String, onSuccessResponse: (List<Hero>) -> Unit) {
+
+    if (superheroName.isBlank())
+        return
+    val url = "https://www.superheroapi.com/api.php/6ec08c1c3893984a663d141a671811d1/search/$superheroName"
+
+    CoroutineScope(Dispatchers.IO).launch {
+        val response = NetworkUtils.httpClient.get(url).body<ApiResponse>()
+        onSuccessResponse(response.results)
     }
 }
